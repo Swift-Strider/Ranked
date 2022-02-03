988afd8dd6364bbee2a23cbc496841d735ff7f28
DiamondStrider1 <62265561+Swift-Strider@users.noreply.github.com>
2022-02-03 17:32:34 -0600

    fix Loader

--- src/DiamondStrider1/Ranked/Loader.php
+++ src/DiamondStrider1/Ranked/Loader.php
@@ -28,11 +28,13 @@ declare(strict_types=1);
 
 namespace DiamondStrider1\Ranked;
 
-class Loader
+use pocketmine\plugin\PluginBase;
+
+class Loader extends PluginBase
 {
     private static self $instance;
 
-    public function onLoad()
+    public function onLoad(): void
     {
         self::$instance = $this;
     }
