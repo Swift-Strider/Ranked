51c96c3cd4757ae2527fe859f2f81895960b64f4
DiamondStrider1 <62265561+Swift-Strider@users.noreply.github.com>
2022-02-21 21:11:16 -0600

    add more structs

---
+++ src/DiamondStrider1/Ranked/struct/RankedPlayer.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ *  ________  ________  ________   ___  __    _______   ________
+ * |\   __  \|\   __  \|\   ___  \|\  \|\  \ |\  ___ \ |\   ___ \
+ * \ \  \|\  \ \  \|\  \ \  \\ \  \ \  \/  /|\ \   __/|\ \  \_|\ \
+ *  \ \   _  _\ \   __  \ \  \\ \  \ \   ___  \ \  \_|/_\ \  \ \\ \
+ *   \ \  \\  \\ \  \ \  \ \  \\ \  \ \  \\ \  \ \  \_|\ \ \  \_\\ \
+ *    \ \__\\ _\\ \__\ \__\ \__\\ \__\ \__\\ \__\ \_______\ \_______\
+ *     \|__|\|__|\|__|\|__|\|__| \|__|\|__| \|__|\|_______|\|_______|.
+ *
+ *    Copyright [2022] [DiamondStrider1]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+declare(strict_types=1);
+
+namespace DiamondStrider1\Ranked\struct;
+
+class RankedPlayer
+{
+    /**
+     * @param string $uuid the player's uuid in hex
+     */
+    public function __construct(
+        private string $uuid,
+        private string $username,
+        private string $displayName
+    ) {
+    }
+
+    public function getUuid(): string
+    {
+        return $this->uuid;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function getDisplayName(): string
+    {
+        return $this->displayName;
+    }
+}
